24b19009f2d408e8d314a7588f10b3a3cd4f576a
LiangGuanYu <153156766@qq.com>
2022-11-26 15:02:07 +0000

    Initial commit

---
+++ CSC8503CoreClasses/GameObject.h
@@ -0,0 +1,110 @@
+#pragma once
+#include "Transform.h"
+#include "CollisionVolume.h"
+
+using std::vector;
+
+namespace NCL::CSC8503 {
+
+	enum ObjectLayer
+	{
+		Default,
+		Cube,
+	};
+
+	class NetworkObject;
+	class RenderObject;
+	class PhysicsObject;
+
+	class GameObject	{
+	public:
+		GameObject(std::string name = "");
+		~GameObject();
+
+		void SetBoundingVolume(CollisionVolume* vol) {
+			boundingVolume = vol;
+		}
+
+		const CollisionVolume* GetBoundingVolume() const {
+			return boundingVolume;
+		}
+
+		bool IsActive() const {
+			return isActive;
+		}
+
+		Transform& GetTransform() {
+			return transform;
+		}
+
+		RenderObject* GetRenderObject() const {
+			return renderObject;
+		}
+
+		PhysicsObject* GetPhysicsObject() const {
+			return physicsObject;
+		}
+
+		NetworkObject* GetNetworkObject() const {
+			return networkObject;
+		}
+
+		void SetRenderObject(RenderObject* newObject) {
+			renderObject = newObject;
+		}
+
+		void SetPhysicsObject(PhysicsObject* newObject) {
+			physicsObject = newObject;
+		}
+
+		const std::string& GetName() const {
+			return name;
+		}
+
+		virtual void OnCollisionBegin(GameObject* otherObject) {
+			//std::cout << "OnCollisionBegin event occured!\n";
+		}
+
+		virtual void OnCollisionEnd(GameObject* otherObject) {
+			//std::cout << "OnCollisionEnd event occured!\n";
+		}
+
+		bool GetBroadphaseAABB(Vector3&outsize) const;
+
+		void UpdateBroadphaseAABB();
+
+		void SetWorldID(int newID) {
+			worldID = newID;
+		}
+
+		int		GetWorldID() const {
+			return worldID;
+		}
+
+		void SetLayer(ObjectLayer objectLayer)
+		{
+			layer = objectLayer;
+		}
+		
+		ObjectLayer GetLayer()
+		{
+			return this->layer;
+		}
+	protected:
+		Transform			transform;
+
+		CollisionVolume*	boundingVolume;
+		PhysicsObject*		physicsObject;
+		RenderObject*		renderObject;
+		NetworkObject*		networkObject;
+		ObjectLayer  layer;
+
+
+		bool		isActive;
+		int			worldID;
+		std::string	name;
+
+		Vector3 broadphaseAABB;
+	};
+}
+
